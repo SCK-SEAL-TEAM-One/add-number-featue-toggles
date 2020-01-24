177b5c1fec572208b4551319db94c8b18ef951ac
smalldog124 <panumars124@gmail.com>
2020-01-24 15:05:41 +0700

    [Added]: เพิ่ม toggel ให้ API api/v1/addNumber

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application as expressApplication } from 'express';
 import bodyParser from 'body-parser'
-import {addArabicNumber, addNumberRoman} from "./add_number";
+import { addArabicNumber, addNumberRoman } from "./add_number";
 
 export default class Application {
     public app: expressApplication // from import type express
@@ -10,30 +10,33 @@ export default class Application {
     constructor(port: number) { //assign value
         this.app = express();
         this.port = port;
-        this.featureDecision = {isEnableNewFeature : true}
+        this.featureDecision = { isEnableNewFeature: false }
 
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
 
         //route -> fn
         this.app.post('/api/v1/addNumberArabic', (request, response) => {
-            response.json({"result":addArabicNumber(request.body)})
+            response.json({ "result": addArabicNumber(request.body) })
         })
 
         this.app.post('/api/v1/addNumberRoman', (request, response) => {
-            response.json({"result":addNumberRoman(request.body)})
+            response.json({ "result": addNumberRoman(request.body) })
         })
 
-        this.app.post('/api/v1/addNumber', (request, response) => {
-            if (this.featureDecision.isEnableNewFeature) {
-                response.json({"result":addNumberRoman(request.body)})
-            } else {
-                response.json({"result":addArabicNumber(request.body)})
-            }
-        })
+        if (this.featureDecision.isEnableNewFeature) {
+            this.app.post('/api/v1/addNumber', (request, response) => {
+                const body = request.body
+                if (isNaN(parseInt(body[0]))){
+                    response.json({ "result": addNumberRoman(body) })
+                }else{
+                    response.json({ "result": addArabicNumber(body) })
+                }
+            })
+        }
     }
 
-    listen(){
+    listen() {
         this.app.listen(this.port, () => {
             console.log(`app listening on port ${this.port}`)
         })
@@ -41,7 +44,7 @@ export default class Application {
 }
 
 class FeatureDecision {
-    public isEnableNewFeature : boolean
+    public isEnableNewFeature: boolean
 
     constructor(isEnableNewFeature: boolean) {
         this.isEnableNewFeature = isEnableNewFeature;
